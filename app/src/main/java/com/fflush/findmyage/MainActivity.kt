f731140b138bff73502849106f6a368eaa542346
Martin <martin.e.castro.88@gmail.com>
2017-10-29 15:47:09 -0300

    Initial commit

---
+++ app/src/main/java/com/fflush/findmyage/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.fflush.findmyage
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+        buttonFindMyAge.setOnClickListener{
+        }
+        */
+    }
+
+    fun buttonFindAgeEvent(view: View){
+        val viewl: View = view
+        var yearOfBirth = editTextYear.text.toString().toInt()
+        var yourAge = Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth
+        textViewShowAge.text = "You are $yourAge years old"
+        viewl.hideKeyboard()
+        }
+
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+
+}
